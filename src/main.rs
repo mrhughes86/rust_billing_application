3e4451fd955ab07e543f815045f24176028fcd80
Mark Hughes <wethestrongapparel@gmail.com>
2023-08-08 00:52:26 -0400

    Update and delete options added.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io;
 
 #[derive(Debug, Clone)]
@@ -7,22 +8,36 @@ pub struct Bill {
 }
 
 pub struct Bills {
-    inner: Vec<Bill>,
+    inner: HashMap<String, Bill>,
 }
 
 impl Bills {
     fn new() -> Self {
         Self {
-            inner: vec![]
+            inner: HashMap::new()
         }
     }
 
     fn add(&mut self, bill: Bill) {
-        self.inner.push(bill)
+        self.inner.insert(bill.name.to_string(), bill);
     }
 
     fn get_all(&self) -> Vec<&Bill> {
-        self.inner.iter().collect()
+        self.inner.values().collect()
+    }
+
+    fn remove(&mut self, name: &str) -> bool {
+        self.inner.remove(name).is_some()
+    }
+
+    fn update(&mut self, name: &str, amount: f64) -> bool {
+        match self.inner.get_mut(name) {
+            Some(bill) => {
+                bill.amount = amount;
+                true
+            }
+            None => false
+        }
     }
 }
 
@@ -40,8 +55,28 @@ fn user_input() -> Option<String> { // must return OWNED String.
     }
 }
 
+// convert string to f64
+fn get_bill_amount() -> Option<f64> {
+    println!("Amount:");
+    loop {
+        let input = match user_input() {
+            Some(input) => input,
+            None => return None,
+        };
+        if &input == "" {
+            return None;
+        }
+        // converts the input String to <f64> with the parse().
+        let parsed_input: Result<f64, _> = input.parse();
+        match parsed_input {
+            Ok(amount) => return Some(amount),
+            Err(_) => println!("Please enter a number"),
+        };
+    }
+}
+
 mod menu {
-    use crate::{user_input, Bill, Bills};
+    use crate::{user_input, get_bill_amount, Bill, Bills};
 
     pub fn add_bill(bills: &mut Bills) {
         println!("Bill Name:");
@@ -49,18 +84,63 @@ mod menu {
             Some(input) => input,
             None => return,
         };
-        let amount = match user_input() {
+        let amount = match get_bill_amount() {
             Some(amount) => amount,
             None => return,
         };
         let bill = Bill { name, amount };
         bills.add(bill);
+        println!("Bill added");
+    }
+
+    pub fn view_bills(bills: &Bills) {
+        for bill in bills.get_all() {
+            println!("{:?}", bill);
+        }
+    }
+
+    pub fn remove_bills(bills: &mut Bills) {
+        for bill in bills.get_all() {
+            println!("{:?}", bill);
+        }
+        println!("Enter bill name to remove");
+        let name = match user_input() {
+            Some(name) => name,
+            None => return
+        };
+        if bills.remove(&name) {
+            println!("BILL HAS BEEN REMOVED.")
+        } else {
+            println!("No Bills To Remove");
+        }
+    }
+
+    pub fn update_bill(bills: &mut Bills) {
+        for bills in bills.get_all() {
+            println!("{:?}", bills);
+        }
+        println!("Enter Bill to Update");
+        let name = match user_input() {
+            Some(name) => name,
+            None => return,
+        };
+        let amount = match get_bill_amount() {
+            Some(amount) => amount,
+            None => return,
+        };
+        if bills.update(&name, amount) {
+            println!("Updated!");
+        } else {
+            println!("Bill not found.")
+        }
     }
 }
 
 enum MainMenu {
     AddBill,
     ViewBill,
+    RemoveBill,
+    UpdateBill,
 }
 
 impl MainMenu {
@@ -68,6 +148,8 @@ impl MainMenu {
         match input {
             "1" => Some(Self::AddBill),
             "2" => Some(Self::ViewBill),
+            "3" => Some(Self::RemoveBill),
+            "4" => Some(Self::UpdateBill),
             _ => None,
         }
     }
@@ -76,20 +158,28 @@ impl MainMenu {
         println!("*** BILL MANAGER ***");
         println!("1. Add Bill");
         println!("2. View Bills");
-        println!();
+        println!("3. Remove Bills");
+        println!("4. Update Bills");
         println!("Enter Selection: ");
     }
 }
 
-fn main() {
+fn run_program() -> Option<()> {
+    let mut bills = Bills::new();
     loop {
-        println!("Welcome to EZ Billing");
         MainMenu::show();
-        let input = user_input().expect("No Data..");
+        let input = user_input()?;
         match MainMenu::from_str(input.as_str()) {
-            Some(MainMenu::AddBill) => (),
-            Some(MainMenu::ViewBill) => (),
-            None => return
+            Some(MainMenu::AddBill) => menu::add_bill(&mut bills),
+            Some(MainMenu::ViewBill) => menu::view_bills(&bills),
+            Some(MainMenu::RemoveBill) => menu::remove_bills(&mut bills),
+            Some(MainMenu::UpdateBill) => menu::update_bill(&mut bills),
+            None => break
         }
     }
+    None
+}
+
+fn main() {
+    run_program();
 }
